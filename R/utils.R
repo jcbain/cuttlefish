39b1b0fb2cbc37e45612f668e103b97d8614081f
jcbain <jamescbain@gmail.com>
2018-07-12 00:09:15 -0500

    find unique hex colors

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,13 @@
 #'
 #' @param image The image to load.
 extract_colors <- function(image){
+  img = magick::image_read(image)
+  bitmap = img[[1]]
+  hexcomps = apply(bitmap, MARGIN=1, FUN=function(x) x)
+  hexes = unique(paste0("#", hexcomps[,1], hexcomps[,2], hexcomps[,3]))
+
+  hexes
 
 }
 
-rgb2hex <- function(r,g,b) rgb(r, g, b, maxColorValue = 255)
+
