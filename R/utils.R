c9eef4354e704292896fece9ed85cf8a58d82683
jcbain <jamescbain@gmail.com>
2018-07-12 21:45:23 -0500

    return an rgb matrix

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,7 @@
 #'
 #' `extract_colors` takes in an image and returns all of the
 #' unique colors in the image in RGB color-space.
-#' 
+#'
 #' @param image The image to load.
 #' @return Returns a tibble with three columns refering to
 #'     red, green and blue states of the pixel.
@@ -12,7 +12,8 @@ extract_colors <- function(image){
   hexcomps = apply(bitmap, MARGIN=1, FUN=function(x) x)
   hexes = unique(paste0("#", hexcomps[,1], hexcomps[,2], hexcomps[,3]))
 
-  dplyr::as_tibble(t(col2rgb(hexes)))
+  rgb <- t(col2rgb(hexes))
+  apply(X = rgb, MARGIN = 2, FUN = as.numeric)
 }
 
 
