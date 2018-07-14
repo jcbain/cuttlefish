ebe3781d08b80fbe61e28b0b6962f565e29ca9c6
jcbain <jamescbain@gmail.com>
2018-07-14 00:21:27 -0500

    modify function to find max index in the matrix at hand

--- R/distinctions.R
+++ R/distinctions.R
@@ -16,10 +16,11 @@ euclid_distance <- function(x, y){
 #' @param x Matrix of RGB colors.
 #' @param n `n` distinct colors to be output.
 distinct_hsv <- function(x, n){
-  angle =   360/n
-
   rgbmat = colorspace::RGB(x)
   hsv = colorspace::coords(as(rgbmat, "HSV"))
+
+  max_angle = max(hsv[,3])
+  angle =   max_angle/n
 
   start_ind = sample(1:nrow(hsv), 1)
 
@@ -27,17 +28,17 @@ distinct_hsv <- function(x, n){
   selection[1,] = hsv[start_ind, ]
 
   for(i in 2:n){
-    current_angle = selection[i - 1, 3] + angle
-    if(current_angle > 360){
-      current_angle = current_angle - 360
+    current_angle = selection[(i - 1), 3] + angle
+    if(current_angle > max_angle){
+      current_angle = current_angle - max_angle
     }
 
-    min_ind = which.min(hsv[,3] - current_angle)
+    min_ind = which.min(euclid_distance(current_angle, hsv[,3]))
     closest_angle = hsv[min_ind, 3]
     options = hsv[hsv[,3]==closest_angle,]
 
     farthest_ind = which.max(as.matrix(pdist::pdist(selection[i-1,1:2], options[,1:2])))
-    selection[1,] = options[furthest_ind,]
+    selection[i,] = options[farthest_ind,]
     # find all values that are closest to new angle
     # select the one that is the farthest distance
     # assign that row to slection[i, ]
