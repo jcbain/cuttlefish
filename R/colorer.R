47d61168cd13d25c94f216e3843af9064af7b147
jcbain <jamescbain@gmail.com>
2018-07-25 21:16:10 -0500

    move all to colorer

--- R/colorer.R
+++ R/colorer.R
@@ -1,9 +1,38 @@
+#' Calculate the Distance between a Point and a Vector
+#'
+#' Takes a point `x` and calculates the euclidean distance from a
+#' vector `y`.
+#'
+#' @param x A numerical value.
+#' @param y A numerical value or vector.
+#' @return A vector of distances
+euclid_distance <- function(x, y){
+  sqrt((y-x)^2)
+}
+
+#' Find Unique RGB Colors in an Image
+#'
+#' `extract_colors` takes in an image and returns all of the
+#' unique colors in the image in RGB color-space.
+#'
+#' @param image The image to load.
+#' @return Returns a matrix with three columns refering to
+#'     red, green and blue states of the pixel.
+extract_colors <- function(image){
+  img = magick::image_read(image)
+  bitmap = img[[1]]
+  hexcomps = apply(bitmap, MARGIN=1, FUN=function(x) x)
+  hexes = unique(paste0("#", hexcomps[,1], hexcomps[,2], hexcomps[,3]))
+
+  rgb <- t(col2rgb(hexes))
+  apply(X = rgb, MARGIN = 2, FUN = as.numeric)
+}
+
 #' Find the n Most Distinct Colors
 #'
 #' `distint_hsv` selects the most distinct set of colors from a set of colors.
 #' @param x Matrix of RGB colors.
 #' @param n `n` distinct colors to be output.
-#' @export
 distinct_hsv <- function(x, n){
 
   # convert to matrix to RBG then to HSV
