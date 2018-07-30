40f9bb6e684e7af99306269243de418c4eee741d
jcbain <jamescbain@gmail.com>
2018-07-29 23:38:26 -0500

    write some tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(cuttlefish)
+
+test_check("cuttlefish")
---
+++ tests/testthat/test_create_palette.R
@@ -0,0 +1,14 @@
+context("Create Palettes")
+library(cuttlefish)
+
+img_file <- "https://jeroen.github.io/images/frink.png"
+
+test_that("prominent.ord option works when turned on", {
+  expect_equal(create_palette(img_file, 5, prominent.ord = T), c("#000000", "#ffffff", "#ffd521", "#f387b8", "#d8e3e9"))
+})
+
+set.seed(1)
+
+test_that("prominent.ord option works when turned off and defaults to angle segmentations", {
+  expect_equal(create_palette(img_file, 5), c("#FCF598", "#3B3A32", "#FCFDFE", "#DEE1E0", "#FDCE20"))
+})
