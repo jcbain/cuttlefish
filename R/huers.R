e8d0c63e3bd5fe18a190c527595b920b297afce3
jcbain <jamescbain@gmail.com>
2018-08-23 23:25:12 -0500

    subset based off of hue

--- R/huers.R
+++ R/huers.R
@@ -1,7 +1,21 @@
 '%ni%' <- Negate('%in%')
 
+hues <- list('red' = c(0, 60), 'yellow' = c(60, 120), 'green' = c(120, 180),
+             'cyan' = c(180, 240), 'blue' = c(240, 300), 'magenta' = c(300, 360))
+
 select_hue <- function(hexes, hue){
-  if (hue %ni% c('red', 'blue')){
+  if (hue %ni% c('red', 'yellow', 'green', 'cyan', 'blue', 'magenta')){
     stop(paste(hue, "is not a selectable hue."))
   }
+  degrees = get(hue, hues)
+
+  uniq_hexes = unique(hexes)
+  tmp_rgb = t(col2rgb(uniq_hexes))
+  tmp_rgb = apply(X = tmp_rgb, MARGIN = 2, FUN = as.numeric)
+
+  # convert to matrix to RBG then to HSV
+  rgbmat = colorspace::RGB(tmp_rgb)
+  tmp_hsv = colorspace::coords(as(rgbmat, "HSV"))
+
+  tmp_hsv[tmp_hsv[,1] >= degrees[1] & tmp_hsv[,1] < degrees[2],]
 }
